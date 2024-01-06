e241e9f5f419558a15a7d7cda2a3d8570df75675
snoopy <angelos81@gmail.com>
2024-01-06 17:22:55 +0900

    도메인 모델

---
+++ src/main/java/com/msa/book/domain/model/Book.java
@@ -0,0 +1,61 @@
+package com.msa.book.domain.model;
+
+import com.msa.book.domain.model.vo.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    private long no;
+    private String title;
+    private BookDesc desc;
+    private Classfication classfication;
+    private BookStatus bookStatus;
+    private Location location;
+
+    // 도서입고
+    public static Book enterBook(String title
+                               , String author
+                               , String isbn
+                               , String description
+                               , LocalDate publicationDate
+                               , Source source
+                               , Classfication classfication
+                               , Location location) {
+
+        BookDesc bookDesc = BookDesc.createBookDesc(author, isbn, description, publicationDate, source);
+
+        Book book = new Book();
+        book.setTitle(title);
+        book.setDesc(bookDesc);
+        book.setClassfication(classfication);
+        book.setLocation(location);
+        book.setBookStatus(BookStatus.ENTERED);
+        return book;
+    }
+
+    public Book makeAvailable() {
+        this.setBookStatus(BookStatus.AVAILABLE);
+        return this;
+    }
+
+    public Book makeUnAvailable() {
+        this.setBookStatus(BookStatus.UNAVAILABLE);
+        return this;
+    }
+
+
+    public static Book sample() {
+        return enterBook("MSA", "홍길동", "123123", "실습으로 배우는 MSA", LocalDate.now(), Source.DONATION, Classfication.COMPUTER, Location.JEONGJA);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Book.sample());
+    }
+
+}
---
+++ src/main/java/com/msa/book/domain/model/vo/BookDesc.java
@@ -0,0 +1,35 @@
+package com.msa.book.domain.model.vo;
+
+import com.msa.book.domain.model.Book;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDesc {
+    private String description;
+    private String author;
+    private String isbn;
+    private LocalDate publicationDate;
+    private Source source;
+
+    public static BookDesc createBookDesc(String author,
+                                          String isbn,
+                                          String description,
+                                          LocalDate publicationDate,
+                                          Source source) {
+        return new BookDesc(description, author, isbn, publicationDate, source);
+    }
+
+    public static BookDesc sample() {
+        return createBookDesc("홍길동", "123123", "실습으로 배우는 MSA", LocalDate.now(), Source.DONATION);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(BookDesc.sample());
+    }
+}
