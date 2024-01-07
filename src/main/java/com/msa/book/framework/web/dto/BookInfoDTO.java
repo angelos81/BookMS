2468ab50f9a57bd4d3b870afb852b83079356fb3
snoopy <angelos81@gmail.com>
2024-01-07 15:43:03 +0900

    통합테스트 설정 변경

--- src/main/java/com/msa/book/framework/web/dto/BookInfoDTO.java
+++ src/main/java/com/msa/book/framework/web/dto/BookInfoDTO.java
@@ -1,10 +1,14 @@
 package com.msa.book.framework.web.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Setter
 @Getter
+@AllArgsConstructor
 public class BookInfoDTO {
     private String title;
     private String description;
